d4b0d1bb0803362b6b8d88396989ca4656fdc2be
Gabriel <gap5991@hotmail.com>
2018-08-09 16:21:29 -0500

    ig

--- acciones/actualizar_equijugador.php
+++ acciones/actualizar_equijugador.php
@@ -2,7 +2,7 @@
 require_once("../clases/cls_equipo.php");
 $equi = new equipo();
 $equi->actualizarpas($_POST['txt_idtransferencia'],$_POST['txt_equipo']);
-if ($arbi)
+if ($equi)
     {
         
         header('Location: ../vistas/frm_equijugador.php');
