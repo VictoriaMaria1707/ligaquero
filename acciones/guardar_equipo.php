f7dd0ff3f57836c3ff31039b9c0a8d57fe393ed1
VictoriaMaria1707 <38673421+VictoriaMaria1707@users.noreply.github.com>
2018-08-13 11:17:33 -0500

    sdf
    dfs

--- acciones/guardar_equipo.php
+++ acciones/guardar_equipo.php
@@ -1,7 +1,11 @@
 <?php
 require_once("../clases/cls_equipo.php");
 $equi = new equipo();
-
+if ($_POST['txt_numjuga'] >15 || $_POST['txt_numjuga'] < 12 ){
+    
+    echo "El equipo solo puede tener hasta 15 jugadores y no menos de 12";
+     header('Refresh: 2; ../vistas/frm_equipo.php');
+}else{
 $equi->insert($_POST['txt_nomequipo'],$_POST['txt_numjuga'],$_POST['txt_nomdue'],$_POST['txt_nomentre'],$_POST['txt_categoria']);
     if ($equi)
     {
@@ -11,5 +15,5 @@ $equi->insert($_POST['txt_nomequipo'],$_POST['txt_numjuga'],$_POST['txt_nomdue']
     else
     {
         echo "error";
-    }
+    }}
 ?>
